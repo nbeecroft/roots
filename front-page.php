3610c85f51665543c10843444bb11cd89368a0d6
Nick Beecroft <nicolas.beecroft@gmail.com>
2012-04-10 04:12:29 +0200

    Added programatically generated links to parent categories

--- front-page.php
+++ front-page.php
@@ -5,19 +5,18 @@
       <div id="main" class="<?php echo FULLWIDTH_CLASSES; ?>" role="main">
       	<h1 class="banner-text">Hi, I'm Nick. What brings you here?</h1>
 	   <section>
-	     <div class="row buttons">
-	       <h2 id="business" class="well span2 offset3">Business</h3>
-	       <h2 id="pleasure" class="well span2 offset1">Pleasure</h3>
+	     <div class="row">
+		<h2 id="business" class="well span2 offset3"><a href="<?php echo get_category_link(18); ?>">Business</a></h2>
+		<h2 id="pleasure" class="well span2 offset1"><a href="<?php echo get_category_link(20); ?>">Pleasure</a></h2>
 	     </div>
-	     <div id="category-list-business" class="row buttons">
-	       <ul class="well span2">
-	         <?php wp_list_categories('child_of=18&hide_empty=0&title_li='); ?>
-    	       </ul>
-             </div>
-             <div id="category-list-pleasure"> 
-	       <ul class="well span2">
-	         <?php wp_list_categories('child_of=20&hide_empty=0&title_li='); ?>
-               </ul>
+	     <div class="child-categories row">
+	       <ul id="category-list-business" class="span5 offset1">
+		 <?php echo my_list_categories(array('child_of'=>18,'hide_empty'=>0,'title_li'=>''));?>
+		</ul>
+	     
+	       <ul id="category-list-pleasure" class="span5">
+		 <?php echo my_list_categories(array('child_of'=>20,'hide_empty'=>0,'title_li'=>''));?>
+	       </ul>
 	     </div>
 	   </section>
       </div><!-- /#main -->
--- inc/roots-custom.php
+++ inc/roots-custom.php
@@ -1,1 +1,22 @@
 <?php
+
+function my_list_categories( $args ) {
+
+	// get category objects
+	$categories = get_categories($args);
+
+	// set vars
+	$output = '';
+
+	// loop objects
+	foreach($categories as $category) {
+
+		// build output
+		$output .= '<li class="' . "well span1" . '">';
+		$output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a>';
+		$output .= '</li>';	
+	} 
+
+	// return output
+	return $output;
+}?>
