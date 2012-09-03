d03687b0180334a6aabd59fd645cb12aedabcc6b
Nick Beecroft <nicolas.beecroft@tidepowerd.com>
2012-09-02 21:08:51 -0700

    Fixed frontpage Articles to only published

--- front-page.php
+++ front-page.php
@@ -57,7 +57,8 @@
                 <h2>Recently Written</h2>
                 <ul>
                 <?php
-                    $args = array( 'numberposts' => '3' );
+                    $args = array( 'numberposts' => '3',
+                        'post_status'=>'publish' );
                     $recent_posts = wp_get_recent_posts( $args );
                     foreach( $recent_posts as $recent ){
                         echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
